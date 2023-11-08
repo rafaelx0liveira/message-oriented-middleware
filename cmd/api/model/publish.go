eba83e923fbb5a26a19a55bfd54ff736515c5ae2
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-11-07 22:11:47 -0300

    feat: publish model

---
+++ cmd/api/model/publish.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"github.com/rafaelx0liveira/message-oriented-middleware/cmd/api/util"
+)
+
+type Publish struct {
+	Content string `json:"content"`
+}
+
+func (p *Publish) Validate () error {
+	if p.Content == "" {
+		return util.ErrParamIsRequired("Content", "string")
+	}
+
+	return nil
+}
