6a254479fad896a4d580fa41be97e8d4a7d3f536
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-11-09 18:29:58 -0300

    feat: utility function for response

--- cmd/api/util/responseUtil.go
+++ cmd/api/util/responseUtil.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +11,13 @@ func SendError(c *gin.Context, status int, message string) {
 	c.Header("Content-Type", "application/json")
 	c.JSON(status, gin.H{
 		"error": message,
+	})
+}
+
+func SendSuccess(c *gin.Context, operation string, data interface{}) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s successfully", operation),
+		"data":    data,
 	})
 }
