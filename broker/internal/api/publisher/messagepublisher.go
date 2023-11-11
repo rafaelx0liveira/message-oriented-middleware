a6dc972a91b2cd68748b94653268e892c2f6f51d
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-11-10 23:16:04 -0300

    fix: adding a messagepublisher

---
+++ broker/internal/api/publisher/messagepublisher.go
@@ -0,0 +1,24 @@
+package publisher
+
+import (
+	"broker/internal"
+	"broker/internal/api/config"
+	"broker/internal/api/model"
+)
+
+func PublishMessage(message *model.Message, logger *config.Logger) error{
+	broker := internal.NewBroker()
+
+	newMessage := internal.NewMessage(message.ID, message.Content)
+
+	err := broker.SendMessage(newMessage)
+
+	if err != nil {
+		logger.Errorf("Error while publishing message: %s", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+
